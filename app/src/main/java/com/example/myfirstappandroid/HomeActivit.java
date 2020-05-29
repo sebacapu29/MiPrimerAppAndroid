ba6e3f3453f461116ddcc88df539f479da725597
= <=>
2020-05-29 19:07:16 -0300

    Agrego listview, adapter, renderiza lista de string, corrijo toast

--- app/src/main/java/com/example/myfirstappandroid/HomeActivit.java
+++ app/src/main/java/com/example/myfirstappandroid/HomeActivit.java
@@ -3,12 +3,56 @@ package com.example.myfirstappandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class HomeActivit extends AppCompatActivity {
+import java.util.List;
+
+public class HomeActivit extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    ListView lista;
+    List<String> strLista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+        //Conectar la variable lista con el componente visual ListView del Activity
+        lista = findViewById(R.id.listViewJuegos);
+
+        strLista.add("GTA V");
+        strLista.add("LoL");
+        strLista.add("Fifa 2020");
+        strLista.add("PES 2020");
+        strLista.add("COD Black Ops II");
+        strLista.add("SW Battlefront II");
+        strLista.add("Spyro");
+
+        //Se asigna al Obj Bundle lo que recibimos en Intent (Es decir, lo que datos que pasamos desde el ActivityMain)
+        Bundle extras = getIntent().getExtras();
+
+        String email = extras.getString("email");
+
+        //Setear el titulo del activity
+        setTitle(email);
+
+        //Adapter, convierte la lista de strings en elementos visualizables basandose en una plantilla o layout que va se va a realizar para definir como se mostraran los datos
+        ArrayAdapter adaptadorListaJuegos = new ArrayAdapter(this,android.R.layout.simple_list_item_1);
+
+        //Vincular el listview con el adaptador
+        lista.setAdapter(adaptadorListaJuegos);
+
+        //Gestion de evento click sobre los elementos de listview
+        lista.setOnItemClickListener(this);
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        //String seleccionado = lista.getposition
     }
 }
--- app/src/main/java/com/example/myfirstappandroid/MainActivity.java
+++ app/src/main/java/com/example/myfirstappandroid/MainActivity.java
@@ -26,30 +26,41 @@ public class MainActivity extends AppCompatActivity implements  View.OnClickList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Identificar el control de Android por el ID que se le setea en las propertis (R= Resourses, id property, eTxtMail = nombre del Id)
         txtMail =  findViewById(R.id.eTxtMail);
         txtClave =  findViewById(R.id.eTxtClave);
         btnIniciar = findViewById(R.id.btnIniciar);
 
-        btnIniciar.setOnClickListener(this::onClick);
+        btnIniciar.setOnClickListener(this::onClick);//Listener para asignarle una funcion al evento del boton
     }
 
     @Override
     public void onClick(View v) {
+        //Instancia el obj intent le pasa el context this (clase) y la clase donde va a dirigirse
         Intent intent = new Intent(this, HomeActivit.class);
-        String email = this.txtMail.getText().toString();
+        String email = this.txtMail.getText().toString();//Obtener los valores de los controles
         String clave = this.txtClave.getText().toString();
+        Toast toast1;
 
         if(email.isEmpty()){
-            Toast.makeText(this,"Debe poner un email",Toast.LENGTH_LONG);
+            toast1 = Toast.makeText(this,"Debe poner un email",Toast.LENGTH_LONG);
+            toast1.show();
+            return;
         }
-        else if(email.isEmpty()){
-            Toast.makeText(this,"Debe poner un contraseña",Toast.LENGTH_LONG);
+        else if(clave.isEmpty()){
+            toast1 = Toast.makeText(this,"Debe poner un contraseña",Toast.LENGTH_LONG);
+            toast1.show();
+            return;
         }
-        else{
-            Toast.makeText(this,"Debe completar los campos",Toast.LENGTH_LONG);
+        else if(email.isEmpty()&& clave.isEmpty()){
+            toast1 = Toast.makeText(this,"Debe completar los campos",Toast.LENGTH_LONG);
+            toast1.show();
+            return;
         }
+
+        //Enviar parametros a la otra vista
         intent.putExtra("email", email);
 
-        startActivity(intent);
+        startActivity(intent);//Lanza el activity
     }
 }
